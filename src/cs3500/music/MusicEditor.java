7b732baf3df64c7ed0dc3d3d8c937cdb0e087537
Viviano Cantu <viviano.cantu@gmail.com>
2016-03-24 21:28:12 -0400

    jar works with args now

--- src/cs3500/music/MusicEditor.java
+++ src/cs3500/music/MusicEditor.java
@@ -8,6 +8,7 @@ import cs3500.music.model.MusicModel;
 import cs3500.music.util.GenericMusicModelBuilder;
 import cs3500.music.util.ModelIOCreator;
 import cs3500.music.util.MusicReader;
+import cs3500.music.util.MusicViewFactory;
 import cs3500.music.view.*;
 
 
@@ -17,9 +18,19 @@ public class MusicEditor {
   public static void main(String[] args)
           throws IOException, InvalidMidiDataException {
 
-    ModelIOCreator console = new ModelIOCreator(
-            new InputStreamReader(System.in), System.out);
-    console.create();
+    if (args.length == 2) {
+      MusicView view = MusicViewFactory.build(args[0]);
+      MusicModel model = MusicReader.parseFile(
+              new FileReader(args[1]),
+              new GenericMusicModelBuilder());
+      view.setModel(new ModelDisplayAdapterImpl(model));
+      view.initialize();
+    }
+    else {
+      ModelIOCreator console = new ModelIOCreator(
+              new InputStreamReader(System.in), System.out);
+      console.create();
+    }
   }
 
 }
